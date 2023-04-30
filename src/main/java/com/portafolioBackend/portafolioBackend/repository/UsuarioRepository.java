501764264cbe15e78f1113e121b94306df43629e
Valen <valenreynoso17@gmail.com>
2023-04-30 11:50:04 -0300

    feat: agregar comportamiento en usuario repository

--- src/main/java/com/portafolioBackend/portafolioBackend/repository/UsuarioRepository.java
+++ src/main/java/com/portafolioBackend/portafolioBackend/repository/UsuarioRepository.java
@@ -2,8 +2,15 @@ package com.portafolioBackend.portafolioBackend.repository;
 
 import com.portafolioBackend.portafolioBackend.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    @Query("SELECT u FROM Usuario u WHERE u.email = :email")
+    Usuario getUsuariosPorEmail(@Param("email") String email);
 }
